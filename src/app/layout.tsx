96a576565848975ecbf8fd536815ce48a80ac542
Nikola-1 <dndjuric73@gmail.com>
2025-12-10 11:44:09 +0100

    Update tsconfig

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,12 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <link
+  href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:wght@400;700&display=swap"
+  rel="stylesheet"
+/>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
